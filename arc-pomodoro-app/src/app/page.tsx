d816c182b7e5fdc525a3efa9a3bfcb600417f802
Aldrich Agabin <ArcFlu52@gmail.com>
2024-11-26 21:09:58 -0600

    Dynamic Opacity and more Buttons

--- arc-pomodoro-app/src/app/page.tsx
+++ arc-pomodoro-app/src/app/page.tsx
@@ -1,18 +1,21 @@
 import ToggleModeButton from '@/components/ui/ToggleModeButton';
+import { Button } from '@mui/material';
 
 export default function Home() {
   return (
-    <div className='bg-background text-text flex min-h-screen flex-col items-center justify-center'>
-      <header className='bg-background w-full p-4 text-center shadow-md'>
+    <div className='text-text flex min-h-screen flex-col items-center justify-center bg-background'>
+      <header className='w-full bg-background p-4 text-center shadow-md'>
         <h1 className='text-3xl font-bold'>Welcome to Arc-Pomodoro</h1>
       </header>
 
       <main className='mt-10 flex flex-col items-center space-y-6'>
         <p className='text-lg'>This is a simple homepage.</p>
         <ToggleModeButton />
+        <Button> redirect to the pomodoro page</Button>
+        <p>also not implemented yet</p>
       </main>
 
-      <footer className='bg-background mt-auto w-full p-4 text-center'>
+      <footer className='mt-auto w-full bg-background p-4 text-center'>
         <p className='text-sm'>bruh moment footer type beat</p>
       </footer>
     </div>
