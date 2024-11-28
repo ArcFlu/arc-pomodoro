909beea7a72bea380fa4f8bd665a1d393a1ba080
allanc3500 <allanc3500@gmail.com>
2024-11-28 09:08:12 -0600

    add buttons and time functionality

--- arc-pomodoro-app/src/app/pomodoro/Timer.tsx
+++ arc-pomodoro-app/src/app/pomodoro/Timer.tsx
@@ -1,21 +1,49 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 
+  
 const Timer = (
   props: React.JSX.IntrinsicAttributes &
     React.ClassAttributes<HTMLParagraphElement> &
     React.HTMLAttributes<HTMLParagraphElement>
 ) => {
   const [timeMS, setTimeMS] = useState(100);
+  const [disable, setDisable] = useState(true);
+  const [toggleTime, setTimeName] = useState("Start Timer");
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimeMS(timeMS - 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [timeMS]);
+  const handleOnClickStop = () =>{
+    setDisable(prevValue => !prevValue);
+    console.log(disable);
+    if(!disable){
+      setTimeName("Start Timer");
+    }
+    else{
+      setTimeName("Stop Timer");
+    }
+    return disable;
+  }
 
-  return <p {...props}>{timeMS}</p>;
+  const handleOnClickReset = () => {
+    setTimeMS(100);
+    setDisable(true);
+    setTimeName("Start Timer");
+  }
+
+  useEffect(() => { 
+    if(disable === false){
+        const interval = setInterval(() => {
+          setTimeMS(timeMS - 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }
+  }, [timeMS, disable]);
+
+  return <>
+  <p {...props}>{timeMS}</p>
+  <button onClick = {handleOnClickReset}>Reset</button>
+  <br></br>
+  <button onClick = {handleOnClickStop}>{toggleTime}</button>
+  </>;
 };
 
 export default Timer;
