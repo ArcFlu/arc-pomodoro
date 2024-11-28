fd031d3c24f42ee2ae297de26eca3d1de2b73e95
Aldrich Agabin <ArcFlu52@gmail.com>
2024-11-27 19:59:54 -0600

    Implemented Start, Stop, and Set Timer

--- arc-pomodoro-app/src/app/forest-clone/page.tsx
+++ arc-pomodoro-app/src/app/forest-clone/page.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
+import DisplayForestStatsSection from './DisplayForestStatsSection';
+import TimerAndControlsSection from './TimerAndControlsSection';
 
 const Page: React.FC = () => {
   return (
     <div className='h-screen p-5'>
-      <div className='flex h-1/3 items-center justify-center border-4 border-blue-500'>
-        <h1 className='text-xl text-foreground'>Top Section</h1>
-      </div>
-
-      <div className='grid h-2/3 grid-cols-2'>
-        <div className='flex items-center justify-center border-4 border-green-500'>
-          <h1 className='text-xl text-foreground'>Left Section</h1>
-        </div>
-        <div className='flex items-center justify-center border-4 border-red-500'>
-          <h1 className='text-xl text-foreground'>Right Section</h1>
-        </div>
-      </div>
+      <DisplayForestStatsSection />
+      <TimerAndControlsSection />
     </div>
   );
 };
