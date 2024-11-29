1d6c34ef6b2d745097a7c4f22d0b54e171174545
Aldrich Agabin <ArcFlu52@gmail.com>
2024-11-29 14:42:54 -0600

    Super Basic Github Auth

--- arc-pomodoro-app/src/app/page.tsx
+++ arc-pomodoro-app/src/app/page.tsx
@@ -1,23 +1,11 @@
-import ToggleModeButton from '@/components/ui/ToggleModeButton';
-import { Button } from '@mui/material';
+'use client';
+import { SessionProvider } from 'next-auth/react';
+import LandingPage from './LandingPage';
 
-export default function Home() {
+export default function App() {
   return (
-    <div className='text-text flex min-h-screen flex-col items-center justify-center bg-background'>
-      <header className='w-full bg-background p-4 text-center shadow-md'>
-        <h1 className='text-3xl font-bold'>Welcome to Arc-Pomodoro</h1>
-      </header>
-
-      <main className='mt-10 flex flex-col items-center space-y-6'>
-        <p className='text-lg'>This is a simple homepage.</p>
-        <ToggleModeButton />
-        <Button> redirect to the pomodoro page</Button>
-        <p>also not implemented yet</p>
-      </main>
-
-      <footer className='mt-auto w-full bg-background p-4 text-center'>
-        <p className='text-sm'>bruh moment footer type beat</p>
-      </footer>
-    </div>
+    <SessionProvider>
+      <LandingPage />
+    </SessionProvider>
   );
 }
